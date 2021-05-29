2e2f327c4a5258cb7189720bd924c5dabc70a3af
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-29 21:18:35 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,9 +1,10 @@
+#To Solve Telebot Suspension problem by @xD_Shashank
 import os 
 import subprocess
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
-basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
+basicConfig(format="𝐓𝐇𝐄 𝐒𝐇𝐀𝐒𝐇𝐀𝐍𝐊 [%(levelname) ✘ ⫸ 5s/%(asctime)s] ⫷ %(name)s: ⛝ %(message)s ⛝", level=INFO)
 LOGS = getLogger("Helper")
-os.system("git clone https://github.com/DARK-COBRA/DARKCOBRA darkcobra")
+os.system("git clone https://github.com/theshashankk/DARKCOBRA darkcobra")
 os.chdir("darkcobra")
 process = subprocess.Popen(
         ["python3", "-m", "userbot"],
